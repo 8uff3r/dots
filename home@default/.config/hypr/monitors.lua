b9ff94c6ac0c82707998c7e93f43d244143d9a27
mise <mise@localhost>
2026-09-08 23:07:19 +0330

    edited hypr/monitors.lua
    Mise-History: {"trigger":"edit","description_source":"computed","task":null,"labels":[],"omitted":[],"incomplete":[],"operation":null}

--- home@default/.config/hypr/monitors.lua
+++ home@default/.config/hypr/monitors.lua
@@ -5,7 +5,7 @@ local omarchy_gdk_scale = 1
 local omarchy_monitor_scale = 1
 
 hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = omarchy_monitor_scale, vrr = false })
+hl.monitor({ output = "", mode = "1920x1080@100", position = "auto", scale = omarchy_monitor_scale, vrr = false })
 
 -- Configure a specific monitor.
 -- hl.monitor({ output = "DP-2", mode = "2560x1440@144", position = "0x0", scale = 1 })
