7c5598f0820bba2464e6cf2dd7dfcaf9aae91a15
mise <mise@localhost>
2026-09-08 19:06:41 +0330

    added hypr/.luarc.json, hypr/autostart.lua, hypr/bindings.lua, hypr/env.lua, hypr/hyprland.lua, hypr/hyprsunset.conf +6 more
    Mise-History: {"trigger":"edit","description_source":"computed","task":null,"labels":[],"omitted":[],"incomplete":[],"operation":null}

---
+++ home@default/.config/hypr/vars.lua
@@ -0,0 +1,19 @@
+local M = {
+    ---------------------
+    ---- MY PROGRAMS ----
+    ---------------------
+    terminal = "foot",
+    fileManager = "flea",
+    menu = "fuzzel",
+    editor = "zeditor",
+    ---------------------
+    ------ CURSORS ------
+    ---------------------
+    cursorTheme = "Bibata-Modern-Classic",
+    cursorSize = "24",
+
+    touchpadDisableTyping = true,
+    touchpadScrollFactor = 0.3,
+}
+
+return M
