1f2a8c4fb94d769fe2a8a5891bb13d9a714c0365
mise <mise@localhost>
2026-09-10 00:25:33 +0330

    edited hypr/looknfeel.lua
    Mise-History: {"trigger":"edit","description_source":"computed","task":null,"labels":[],"omitted":[],"incomplete":[],"operation":null}

--- home@default/.config/hypr/looknfeel.lua
+++ home@default/.config/hypr/looknfeel.lua
@@ -29,7 +29,7 @@ hl.config({
                 inactive = hl.get_config("group.col.border_inactive"),
             },
             indicator_gap = 0,
-            height = 18,
+            height = 15,
             gaps_in = 5,
             gaps_out = 0,
             keep_upper_gap = false,
